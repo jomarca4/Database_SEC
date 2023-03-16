515b8da3a1d39d9a0c31bf412eda00ae2f2eb1c4
jomarca4 <jomarca23@gmail.com>
2023-03-16 23:01:39 +0100

    Commit message

--- read_queries_finance_database.py
+++ read_queries_finance_database.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sqlite3
 
 def read_queries():
-    conn = sqlite3.connect('financial_statements_SEC_EDGAR.db')
+    conn = sqlite3.connect('/Users/josemanuel/Desktop/Python_Scripts/financial_statements_SEC_EDGAR.db')
 
     query= '''
    SELECT DISTINCT companies.name, taxonomy.label, quarters.quarter_number, financial_statement_items.value, financial_statement_items.unit_of_measurement, financial_statements.date
@@ -12,7 +12,7 @@ JOIN financial_statements ON financial_statement_items.financial_statement_id = 
 JOIN quarters ON financial_statements.quarter_id = quarters.id
 JOIN companies ON quarters.company_id = companies.id
 JOIN taxonomy ON taxonomy.name = financial_statement_items.account_label
-WHERE financial_statement_items.account_label LIKE '%even%' AND quarters."year"  =2020 AND financial_statements."type" = 'income_statement'
+WHERE financial_statement_items.account_label LIKE '%even%' AND quarters."year"  =2019 AND financial_statements."type" = 'income_statement'
 ORDER BY financial_statement_items.value DESC;
    
     '''
@@ -36,7 +36,7 @@ ORDER BY financial_statement_items.value DESC;
         # Execute the query and fetch the results
     result = conn.execute(query).fetchall()
     revenues = pd.read_sql_query(query, conn)
-    wb = xw.Book("income_stat_analysis.xlsx")
+    wb = xw.Book("/Users/josemanuel/Desktop/Python_Scripts/income_stat_analysis.xlsx")
     sheet = wb.sheets["Sheet1"]
     sheet.clear()
     sheet.range("A1").value = revenues
