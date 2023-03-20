3e37ee87c7c64da711a4cd6b42efeb0ef09419d4
jomarca4 <jomarca23@gmail.com>
2023-03-20 22:27:47 +0100

    initial commit 2

--- read_queries_finance_database.py
+++ read_queries_finance_database.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sqlite3
 
 def read_queries():
-    conn = sqlite3.connect('/Users/josemanuel/Desktop/Python_Scripts/financial_statements_SEC_EDGAR.db')
+    conn = sqlite3.connect('/Users/josemanuel/Desktop/financial_statements_SEC_EDGAR.db', timeout=10.0, factory=sqlite3.Connection)
     cursor = conn.cursor()
 
     query= '''
@@ -17,7 +17,9 @@ WHERE financial_statement_items.account_label LIKE '%even%' AND quarters."year" 
 ORDER BY financial_statement_items.value DESC;
    
     '''
-
+    query= '''
+       SELECT * from companies
+       '''
     # Define the variables
     company_names = 'Apple%'
     year = 2022
@@ -44,4 +46,47 @@ ORDER BY financial_statement_items.value DESC;
     sheet.range("A1").value = revenues
 
     
-read_queries()
+#read_queries()
+
+def count_records_per_quarter():
+    conn = sqlite3.connect('/Users/josemanuel/Desktop/financial_statements_SEC_EDGAR.db', timeout=10.0, factory=sqlite3.Connection)
+    cursor = conn.cursor()
+
+    query= '''
+       SELECT DISTINCT quarters.quarter_number, quarters.year, companies.name, financial_statements.type, financial_statement_items.account_label
+       FROM companies
+       JOIN quarters ON quarters.company_id = companies.id
+       JOIN financial_statements ON financial_statements.id = quarters.id
+       JOIN financial_statement_items ON financial_statement_items.id = financial_statements.id
+       WHERE companies.name LIKE '%Apple Inc.%'
+       ORDER BY quarters.year
+       '''
+    revenues = pd.read_sql_query(query, conn)
+    print(revenues)
+
+    print(revenues.groupby(['year','quarter_number','type']).count())
+    wb = xw.Book("/Users/josemanuel/Desktop/Python_Scripts/income_stat_analysis.xlsx")
+    sheet = wb.sheets["Sheet1"]
+    sheet.clear()
+    revenues = pd.read_sql_query(query, conn)
+    sheet.range("A1").value = revenues
+count_records_per_quarter()
+
+def count_records():
+    conn = sqlite3.connect('/Users/josemanuel/Desktop/financial_statements_SEC_EDGAR.db', timeout=10.0, factory=sqlite3.Connection)
+    cursor = conn.cursor()
+
+    query= '''
+SELECT quarters.quarter_number, quarters.year, COUNT(*) as num_records
+FROM companies
+JOIN quarters ON quarters.company_id = companies.id
+       JOIN financial_statements ON financial_statements.id = quarters.id
+       JOIN financial_statement_items ON financial_statement_items.id = financial_statements.id
+       WHERE companies.name LIKE '%%'
+GROUP BY quarters.year, quarters.quarter_number
+ORDER BY quarters.year, quarters.quarter_number;
+       '''
+    revenues = pd.read_sql_query(query, conn)
+
+    print(revenues)
+count_records()
